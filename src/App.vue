eab54196820d2d987671f6bc9b31fc7b2c381c2f
Eric Wang <ericfengwang@gmail.com>
2025-05-08 22:39:52 -0500

    feat: initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header
+    class="bg-blue-500 text-white p-6 flex flex-col items-center md:flex-row md:justify-between"
+  ></header>
+
+  <main class="p-6 bg-gray-100 min-h-screen">Test</main>
+</template>
+
+<style scoped>
+/* Remove the existing styles if Tailwind is fully integrated */
+</style>
